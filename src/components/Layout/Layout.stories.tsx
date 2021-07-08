a2d39ca46de5da3691d3ec5d2f13501dd25295cd
Wojciech Malarski <w.malarski@gmail.com>
2021-07-08 19:28:47 +0200

    storybook setup

---
+++ src/components/Layout/Layout.stories.tsx
@@ -0,0 +1,13 @@
+import { ComponentMeta, ComponentStory } from "@storybook/react";
+import React from "react";
+import Layout from "./Layout";
+
+export default {
+  title: "Components/Layout",
+  component: Layout,
+} as ComponentMeta<typeof Layout>;
+
+const Template: ComponentStory<typeof Layout> = (args) => <Layout {...args} />;
+
+export const Primary = Template.bind({});
+Primary.args = { children: "Hello" };
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,6 @@
 import type { AppProps } from "next/app";
-import "../styles/globals.css";
 
-function MyApp({ Component, pageProps }: AppProps): JSX.Element {
-  return <Component {...pageProps} />;
-}
+const MyApp = ({ Component, pageProps }: AppProps): JSX.Element => (
+  <Component {...pageProps} />
+);
 export default MyApp;
