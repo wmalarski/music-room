b360cbefad488b54fcc00be42dfb83f3ccf4c6fc
Wojciech Malarski <w.malarski@gmail.com>
2022-01-23 22:12:02 +0100

    test utils package removed

--- libs/util-tests/jest.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  displayName: 'util-tests',
-  preset: '../../jest.preset.js',
-  transform: {
-    '^.+\\.[tj]sx?$': 'babel-jest',
-  },
-  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
-  coverageDirectory: '../../coverage/libs/util-tests',
-};
