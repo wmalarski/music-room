5c23a810f5a20f19f7727a416181ce596066df0e
Wojciech Malarski <w.malarski@gmail.com>
2022-01-24 23:01:24 +0100

    missed translations added

--- apps/music-room/src/modules/Auth/Auth.tsx
+++ apps/music-room/src/modules/Auth/Auth.tsx
@@ -1,4 +1,5 @@
 import { Tabs, TabsContent, TabsList, TabsTrigger } from '@music-room/ui';
+import { useTranslation } from 'next-i18next';
 import { ReactElement } from 'react';
 import { Layout } from '../Layout/Layout';
 import * as Styles from './Auth.styles';
@@ -6,13 +7,15 @@ import { SignIn } from './SignIn/SignIn';
 import { SignUp } from './SignUp/SignUp';
 
 export const Auth = (): ReactElement => {
+  const { t } = useTranslation('auth');
+
   return (
     <Layout>
       <Styles.Container justifyContent="center" alignItems="center">
         <Tabs defaultValue="signIn">
-          <TabsList aria-label="Sign In or Sign Up">
-            <TabsTrigger value="signIn">Sign In</TabsTrigger>
-            <TabsTrigger value="signUp">Sign Up</TabsTrigger>
+          <TabsList aria-label={`${t('signInHeader')}, ${t('signUpHeader')}`}>
+            <TabsTrigger value="signIn">{t('signInHeader')}</TabsTrigger>
+            <TabsTrigger value="signUp">{t('signUpHeader')}</TabsTrigger>
           </TabsList>
           <TabsContent value="signIn">
             <SignIn />
